53690044ed4d66db7927e1f69bd45d757a04a538
Akbarhusen <akbar.h.sunasara@gmail.com>
2019-06-11 08:35:22 +0530

    Update form.py

--- studentdata/form.py
+++ studentdata/form.py
@@ -7,14 +7,6 @@ class DaTa(forms.ModelForm):
     student_roll_no = forms.IntegerField()
     study_field = forms.CharField()
 
-
-class Meta:
-    model = Student
-    fields = ('student_name', 'student_roll_no', 'study_field')
-
-
-
-
-
-
-
+    class Meta:
+        model = Student
+        fields = ('student_name', 'student_roll_no', 'study_field')
